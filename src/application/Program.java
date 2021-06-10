fd7e3094dcf1b77c6035aa09e809ab3ad8e4fc76
Carlos Eduardo Witte <cewitte@outlook.com>
2021-06-10 16:33:05 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,17 @@
 package application;
 
-import java.sql.Connection;
+import java.util.Date;
 
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
-		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
